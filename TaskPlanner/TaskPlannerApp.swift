d024d4712a80cd71cc9663c156f7276ac9752dbd
Berkay-Disli <berkaydisli.work@gmail.com>
2023-01-21 15:53:16 +0300

    Initial Commit

---
+++ TaskPlanner/TaskPlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskPlannerApp.swift
+//  TaskPlanner
+//
+//  Created by Berkay Disli on 21.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct TaskPlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
